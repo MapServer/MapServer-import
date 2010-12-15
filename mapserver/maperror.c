MapServer/MapServer-import

commit f418ac1155e627ba5636a9b2ddf1aec9a63f70ff

Fixed: Memory allocation results should always be checked (#3559)
git-svn-id: http://svn.osgeo.org/mapserver/trunk@10772 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/maperror.c
@@ -400,9 +400,6 @@ void msWriteErrorImage(mapObj *map, char *filename, int blank) {
   fontMetrics *font = NULL;
   char *imagepath = NULL, *imageurl = NULL;
   labelStyleObj ls;
-  colorObj labelcolor, labeloutlinecolor;
-  ls.color = &labelcolor;
-  ls.outlinecolor = &labeloutlinecolor;
   
   if (map) {
       if( map->width > 0 && map->height > 0 )
@@ -428,7 +425,6 @@ void msWriteErrorImage(mapObj *map, char *filename, int blank) {
 	  if((font = renderer->bitmapFontMetrics[font_index]) != NULL) {
 	     ls.size = i;
 		  MS_INIT_COLOR(*ls.color,0,0,0);
-		  MS_INIT_COLOR(*ls.outlinecolor,255,255,255);
 		  break;
 	  }
   }
@@ -466,7 +462,7 @@ void msWriteErrorImage(mapObj *map, char *filename, int blank) {
 		} else {
 		  nLines = 1;
 		  papszLines = (char **)malloc(nLines*sizeof(char *));
-		  papszLines[0] = strdup(errormsg);
+		  papszLines[0] = msStrdup(errormsg);
 		}   
 		for (i=0; i<nLines; i++) {
 		  nYPos = (font->charHeight) * ((i*2) +1); 

commit 672f76e2ba525ae8f8ee1899b6dc7f3bfc9e6c56

fix bug introduced in r10772
git-svn-id: http://svn.osgeo.org/mapserver/trunk@10799 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/maperror.c
@@ -400,6 +400,9 @@ void msWriteErrorImage(mapObj *map, char *filename, int blank) {
   fontMetrics *font = NULL;
   char *imagepath = NULL, *imageurl = NULL;
   labelStyleObj ls;
+  colorObj labelcolor, labeloutlinecolor;
+  ls.color = &labelcolor;
+  ls.outlinecolor = &labeloutlinecolor;
   
   if (map) {
       if( map->width > 0 && map->height > 0 )
@@ -424,8 +427,9 @@ void msWriteErrorImage(mapObj *map, char *filename, int blank) {
 	  /* use the first font we find */
 	  if((font = renderer->bitmapFontMetrics[font_index]) != NULL) {
 	     ls.size = i;
-		  MS_INIT_COLOR(*ls.color,0,0,0);
-		  break;
+         MS_INIT_COLOR(*ls.color,0,0,0);
+         MS_INIT_COLOR(*ls.outlinecolor,255,255,255);
+         break;
 	  }
   }
   /* if no font found we can't do much. this shouldn't happen */