MapServer/MapServer-import

commit 2b95033a294afb734489d7bddd5f5d63827c7196

Make sure symbols defined in a mapfile are not written when saving a symbolset (related to #3589).
git-svn-id: http://svn.osgeo.org/mapserver/trunk@11844 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/mapsymbol.c
@@ -725,16 +725,16 @@ symbolObj *msRemoveSymbol(symbolSetObj *symbolset, int nSymbolIndex) {
 }
 
 int msSaveSymbolSetStream(symbolSetObj *symbolset, FILE *stream) {
-    int i;
-    if (!symbolset || !stream) {
-        msSetError(MS_SYMERR, "Cannot save symbolset.", "msSaveSymbolSetStream()");
-        return MS_FAILURE;
-    }
-    /* Don't ever write out the default symbol at index 0 */
-    for (i=1; i<symbolset->numsymbols; i++) {
-        writeSymbol((symbolset->symbol[i]), stream);
-    }
-    return MS_SUCCESS;
+  int i;
+  if (!symbolset || !stream) {
+    msSetError(MS_SYMERR, "Cannot save symbolset.", "msSaveSymbolSetStream()");
+    return MS_FAILURE;
+  }
+  /* Don't ever write out the default symbol at index 0 */
+  for (i=1; i<symbolset->numsymbols; i++) {
+    if(!map->symbolset.symbol[i]->inmapfile) writeSymbol((symbolset->symbol[i]), stream);
+  }
+  return MS_SUCCESS;
 }
 
 int msSaveSymbolSet(symbolSetObj *symbolset, const char *filename) {

commit 3c31e47676963b0d1d0edc946db0909f8311684c

Fix build error introduced in r11844 (#3589)
git-svn-id: http://svn.osgeo.org/mapserver/trunk@11846 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/mapsymbol.c
@@ -732,7 +732,7 @@ int msSaveSymbolSetStream(symbolSetObj *symbolset, FILE *stream) {
   }
   /* Don't ever write out the default symbol at index 0 */
   for (i=1; i<symbolset->numsymbols; i++) {
-    if(!map->symbolset.symbol[i]->inmapfile) writeSymbol((symbolset->symbol[i]), stream);
+    if(!symbolset->symbol[i]->inmapfile) writeSymbol((symbolset->symbol[i]), stream);
   }
   return MS_SUCCESS;
 }